679f9da541a360e3a0c661c984016b4f60e940e8
MMcUCF <56732704+MMcUCF@users.noreply.github.com>
2019-10-18 13:46:24 -0400

    Add files via upload
    simple "Hello, world" in java

---
+++ Cis4615a.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java Cis4615a
+ *  Hello, World
+ *
+ ******************************************************************************/
+
+public class Cis4615a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
