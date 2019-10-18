c231c2e7a71eea51035b39a4659a9fb4ef6f1333
MMcUCF <56732704+MMcUCF@users.noreply.github.com>
2019-10-18 14:02:30 -0400

    Add files via upload
    Add the phrase " - from the class of CIS4615 - Fall 2019 - UCF " to the output

--- Cis4615a.java
+++ Cis4615a.java
@@ -4,8 +4,11 @@
  *
  *  Prints "Hello, World". By tradition, this is everyone's first program.
  *
+ * Revision History:
+ * - Added " - from the class of CIS4615 - Fall 2019 - UCF"
  *  % java Cis4615a
- *  Hello, World
+ *  Hello, World - from the class of CIS4615 - Fall 2019 - UCF
+ *  %
  *
  ******************************************************************************/
 
@@ -13,7 +16,7 @@ public class Cis4615a {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World - from the class of CIS4615 - Fall 2019 - UCF");
     }
 
 }
