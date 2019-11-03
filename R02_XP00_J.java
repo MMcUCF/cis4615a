e170eaf2ff2349dfd26664d9d6cf0832270a4043
rdickens286 <rdickensucf@gmail.com>
2019-11-03 16:26:36 -0500

    Add files via upload

---
+++ R02_XP00_J.java
@@ -0,0 +1,10 @@
+/*Rule 02. Expressions (EXP)
+XP00-J. Do not ignore values returned by methods 
+Non-Compliant code:
+*/
+public void deleteFile(){
+
+	File someFile = new File("someFileName.txt");
+	//Do something with someFile
+	someFile.delete();
+}
