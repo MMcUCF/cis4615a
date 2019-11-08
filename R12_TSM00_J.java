5205eac789e12ae8dc2455a6d209c4b65ccd299c
rdickens286 <rdickensucf@gmail.com>
2019-11-07 20:25:27 -0500

    Update R12_TSM00_J.java

--- R12_TSM00_J.java
+++ R12_TSM00_J.java
@@ -1,14 +1,18 @@
 /*Rule 12. Thread-Safety Management (TSM)
 TSM00-J. Do not override thread-safe methods with methods that are not thread-safe
-Non-Compliant Code:*/
-class Base {
+Compliant Code:*/
+class R12_TSM00_J {
   public synchronized void doSomething() {
-    // ...
+    System.out.println("Things going on.");
+  }
+
+  public static void main(String [] args){
+    
   }
 }
 
-class Derived extends Base {
-  @Override public void doSomething() {
-    // ...
+class Derived extends R12_TSM00_J {
+  @Override public synchronized void doSomething() {
+    System.out.println("Things also happening.");
   }
 }
