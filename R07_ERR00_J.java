e0320c4bc65ca79ea87c182bcd4773bb1176fc88
rdickens286 <rdickensucf@gmail.com>
2019-11-07 20:07:48 -0500

    Update R07_ERR00_J.java

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,8 +1,26 @@
 /*Rule 07. Exceptional Behavior (ERR)
 ERR00-J. Do not supress or ignore checked expressions.
-Non-Compliant Code:*/
-try {
-  //...
-} catch (IOException ioe) {
-  ioe.printStackTrace();
+Compliant Code:*/
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class R07_ERR00_J{
+  public static void main(String [] args){
+     boolean validFlag = false;
+     File fileName = new File("someFileName.txt");
+    do {
+    try {
+      if(fileName.exists()){
+        validFlag = true;
+      }else{
+        throw new FileNotFoundException();
+      }
+
+  } catch (FileNotFoundException e) {
+    System.out.println("Please specify another file name.");
+    break;
+  }
+} while (validFlag != true);
+  System.out.println("Doing file things.");
+  }
 }
