861c765c196d989cf8f2dd9c41fec4547e91c44a
rdickens286 <rdickensucf@gmail.com>
2019-11-07 19:25:13 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,10 +1,20 @@
 /*Rule 02. Expressions (EXP)
-XP00-J: Do not ignore values returned by methods 
-Non-Compliant code:
-*/
-public void deleteFile(){
+XP00-J: Do not ignore values returned by methods
+Compliant Code:*/
+import java.io.File;
+import java.util.Scanner;
 
-	File someFile = new File("someFileName.txt");
-	//Do something with someFile
-	someFile.delete();
+ public class R02_XP00_J
+{
+	public static void main(String[] args){
+		deleteFile();
+	}
+
+	public static void deleteFile(){
+
+		File someFile = new File("someFileName.txt");
+		if(!someFile.delete()){
+			System.out.println("Error: file did not delete.");
+		}
+	}
 }
