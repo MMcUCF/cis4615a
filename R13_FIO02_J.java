a1d17134e514f562b13b6d662c997ce4b8894d75
rdickens286 <rdickensucf@gmail.com>
2019-11-07 20:30:41 -0500

    Update R13_FIO02_J.java

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -1,5 +1,16 @@
 /*Rule 13. Input Output (FIO)
 FIO02-J. Detect and handle file-related errors
-Non-Compliant Code:*/
-File file = new File(args[0]);
-file.delete();
+Compliant Code:*/
+import java.io.File;
+
+public class R13_FIO02_J{
+
+  public static void main(String [] args){
+    File file = new File("file");
+    if (!file.delete()) {
+        System.out.println("Deletion failed");
+        }
+  }
+
+}
+
