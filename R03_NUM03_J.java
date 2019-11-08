15cab37d8b1c9952485737270dd43221446e5e5d
rdickens286 <rdickensucf@gmail.com>
2019-11-07 20:17:27 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,8 +1,20 @@
 /*
 Rule 03: Numeric Types and Operations (NUM)
 NUM03-J: Use integer types that can fully represent the possible range of unsigned data.
-Non-Compliant code:
-*/
-public static int getInteger(DataInputStream is) throws IOException{
-	return is.readInt();
+Compliant Code:*/
+import java.io.DataInputStream;
+import java.lang.Exceptions;
+
+ public class R03_NUM03_J
+{
+	public static void main(String[] args){
+    DataInputStream is = 34;
+    getInteger(is);
+
+
+	}
+
+	public static long getInteger(DataInputStream is) throws IOException{
+		return is.readInt() & 0xFFFFFFFFL; //Mask with 32 one-bits
+	}
 }
