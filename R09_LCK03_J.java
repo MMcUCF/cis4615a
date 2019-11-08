bcfab920eec350be925f1ae4289fad0496fa0c61
rdickens286 <rdickensucf@gmail.com>
2019-11-07 20:15:01 -0500

    Update R09_LCK03_J.java

--- R09_LCK03_J.java
+++ R09_LCK03_J.java
@@ -1,10 +1,23 @@
 /*Rule 09. Locking (LCK)
 LCK03-J. Do not synchronize on the intrinsic locks of high-level concurrency objects.
 Non-Compliant Code:*/
-private final Lock lock = new ReentrantLock();
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public void doSomething() {
-  synchronized(lock) {
-    // ...
+public class R09_LCK03_J{
+  private final Lock lock = new ReentrantLock();
+
+ public static void main(String [] args){
+   doSomething();
+ }
+
+public static void doSomething() {
+    lock.lock();
+    try {
+      System.out.println("Working on thread.");
+    } finally {
+      lock.unlock();
+    }
   }
 }
+
