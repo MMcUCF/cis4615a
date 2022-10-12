06c5514da8b9678b6387dd789f4b6da8db3890af
Michael McAlpin <56732704+MMcUCF@users.noreply.github.com>
2022-10-12 11:03:29 -0400

    Freshened for 2022

--- Cis4615a.java
+++ Cis4615a.java
@@ -2,7 +2,9 @@
  *  Compilation:  javac Cis4615a.java
  *  Execution:    java Cis4615a
  *
- *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *  Prints 
+ *    "Hello, World - from the class of CIS4615 - Fall 2022 -UCF _YoUR NAME HERE_".
+ *  By tradition, this is everyone's first program.
  *
  * Revision History:
  *   % java Cis4615a
